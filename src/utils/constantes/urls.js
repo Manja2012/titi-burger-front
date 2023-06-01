f7cd39b35460e87e8426e4e93ec6d10461694d05
Marianna Demchenko <mdemko2012@gmail.comm>
2023-06-01 16:37:50 +0200

    localhost2

--- src/utils/constantes/urls.js
+++ src/utils/constantes/urls.js
@@ -1,8 +1,8 @@
 export const URL = {
-    fetchBurger: "http://focused-pike.51-75-140-106.plesk.page/api/products/burger",
-    fetchAccompagnement: "http://focused-pike.51-75-140-106.plesk.page/api/products/accompagnement",
-    fetchBoisson: "http://focused-pike.51-75-140-106.plesk.page/api/products/boisson",
-    fetchDessert: "http://focused-pike.51-75-140-106.plesk.page/api/products/dessert",
-    fetchProduct: "http://focused-pike.51-75-140-106.plesk.page/api/products/id/",
-    fetchPaiement: "http://focused-pike.51-75-140-106.plesk.page/api/paiement"
+    fetchBurger: "https://focused-pike.51-75-140-106.plesk.page/api/products/burger",
+    fetchAccompagnement: "https://focused-pike.51-75-140-106.plesk.page/api/products/accompagnement",
+    fetchBoisson: "https://focused-pike.51-75-140-106.plesk.page/api/products/boisson",
+    fetchDessert: "https://focused-pike.51-75-140-106.plesk.page/api/products/dessert",
+    fetchProduct: "https://focused-pike.51-75-140-106.plesk.page/api/products/id/",
+    fetchPaiement: "https://focused-pike.51-75-140-106.plesk.page/api/paiement"
 }
